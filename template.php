a84dc8425617c73699793139500c3f55b607c2f5
Jason Brown <jason@silex-consulting.com>
2016-03-07 17:11:44 +0000

    Add theme setting alter form function to template.php.

--- template.php
+++ template.php
@@ -4,6 +4,12 @@
  *
  * @see page.tpl.php
  */
-function silex_preprocess_page(&$variables) {
 
+function silex_form_system_theme_settings_alter(&$form, $form_state) {
+    $form['silex_theme-footer-text'] = array(
+        '#type'          => 'textfield',
+        '#title'         => t('Widget'),
+        '#default_value' => theme_get_setting('silex_theme-footer-text'),
+        '#description'   => t("Place this text in the widget spot on your site."),
+    );
 }
