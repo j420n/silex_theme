a5ce8e71fb648ca1c79eaef4fb335cb6826a4b24
Jason Brown <jason@silex-consulting.com>
2016-03-16 15:57:51 +0000

    Add our team view templates.

---
+++ templates/views-view-fields--our-team.tpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<div>
+    <?php if (!empty($fields['field_image_silly']->content) && !empty($_GET['silly']) && $_GET['silly'] == 'very'): ?>
+        <?php if (!empty( $fields['field_image_silly'])): ?>
+            <?php print $fields['field_image_silly']->content ?>
+        <?php endif; ?>
+    <?php else: ?>
+        <?php if (!empty( $fields['field_image'])): ?>
+            <?php print $fields['field_image']->content ?>
+        <?php endif; ?>
+    <?php endif; ?>
+    <?php if (!empty( $fields['title'])): ?>
+        <h1><?php print $fields['title']->content ?></h1>
+    <?php endif; ?>
+    <?php if (!empty( $fields['field_job_title'])): ?>
+        <p><?php print $fields['field_job_title']->content ?></p>
+    <?php endif; ?>
+</div>
